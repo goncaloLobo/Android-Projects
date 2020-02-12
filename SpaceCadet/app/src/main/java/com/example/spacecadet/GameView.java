f89ac91eef170e4d69f054b9a19ce41bcf36f5d5
goncaloLobo <g.ferreiralobo@gmail.com>
2020-02-12 14:58:21 +0000

    Space Cadet -> Gestures Working
    Implementar gestos a andar por 1/3 do ecrã

--- SpaceCadet/app/src/main/java/com/example/spacecadet/GameView.java
+++ SpaceCadet/app/src/main/java/com/example/spacecadet/GameView.java
@@ -1,14 +1,17 @@
 package com.example.spacecadet;
 
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
 public class GameView extends SurfaceView implements Runnable {
 
     private Thread thread;
-    private boolean isPlaying;
+    private boolean isPlaying, onTheGround;
     private int screenX, screenY;
     private Paint paint;
     private Asteroid[] asteroids;
@@ -16,8 +19,23 @@ public class GameView extends SurfaceView implements Runnable {
     public static float screenRatioX, screenRatioY;
     private Background background1, background2;
 
-    public GameView(Context context, int screenX, int screenY) {
-        super(context);
+    private GestureDetector gestureDetector;
+    private GameActivity gameActivity;
+
+    // Minimal x and y axis swipe distance.
+    private static int MIN_SWIPE_DISTANCE_X = 100;
+    private static int MIN_SWIPE_DISTANCE_Y = 100;
+
+    // Maximal x and y axis swipe distance.
+    private static int MAX_SWIPE_DISTANCE_X = 1000;
+    private static int MAX_SWIPE_DISTANCE_Y = 1000;
+
+    private float x1, x2, y1, y2;
+    static final int MIN_DISTANCE = 150;
+
+    public GameView(GameActivity gameActivity, int screenX, int screenY) {
+        super(gameActivity);
+        this.gameActivity = gameActivity;
 
         this.screenX = screenX;
         this.screenY = screenY;
@@ -31,7 +49,8 @@ public class GameView extends SurfaceView implements Runnable {
         background2.y = screenY;
 
         paint = new Paint();
-        rocket = new Rocket(this, screenX, getResources());
+        paint.setColor(getResources().getColor(R.color.blackBackground));
+        rocket = new Rocket(this, screenX, screenY, getResources());
 
         // criacao dos vários asteroids
         asteroids = new Asteroid[3];
@@ -52,6 +71,7 @@ public class GameView extends SurfaceView implements Runnable {
 
     private void update() {
 
+        // movimento do background
         background1.y -= 10 * screenRatioY;
         background2.y -= 10 * screenRatioY;
 
@@ -62,24 +82,51 @@ public class GameView extends SurfaceView implements Runnable {
         if (background2.y + background2.background.getHeight() < 0) {
             background2.y = screenY;
         }
+
+        // movimento do foguetão para os lados
+        if (rocket.isGoingLeft)
+            rocket.x -= 30 * screenRatioX;
+        else
+            rocket.x += 30 * screenRatioX;
+
+        if (rocket.x < 0)
+            rocket.x = 0;
+
+        if (rocket.x >= screenX - rocket.width)
+            rocket.x = screenX - rocket.width;
+
+        // movimento do foguetão para cima e para baixo
+        if (rocket.isGoingUp) {
+            Log.d("GOING UP -> ", "upppppp");
+            rocket.y += (30 * screenRatioY) / 2;
+        } else {
+            rocket.y -= (30 * screenRatioY) / 2;
+        }
+
+        if (rocket.y < 0)
+            rocket.y = 0;
+
+        //if (rocket.y >= screenY - rocket.height)
+        //    rocket.y = screenY - rocket.height;
     }
 
     private void draw() {
 
         if (getHolder().getSurface().isValid()) {
             Canvas canvas = getHolder().lockCanvas();
-            canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
-            canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+            //canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
+            //canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+
+            // resolve a questão de continuamente repetir frames do canvas
+            canvas.drawColor(getResources().getColor(R.color.blackBackground));
 
             // desenhar os vários asteroids
-            for (Asteroid ast : asteroids) {
-                canvas.drawBitmap(ast.getAsteroid(), ast.x, ast.y, paint);
-            }
+            //for (Asteroid ast : asteroids) {
+            //    canvas.drawBitmap(ast.getAsteroid(), ast.x, ast.y, paint);
+            //}
+            //canvas.drawBitmap(asteroids[0].getAsteroid(), asteroids[0].x, asteroids[0].y, paint);
 
-            // coloca o foguetão no centro, em baixo no ecrã
-            int startX1 = (canvas.getWidth() - rocket.getRocket().getWidth()) / 2;
-            int startY1 = (canvas.getHeight() - rocket.getRocket().getHeight() - 50);
-            canvas.drawBitmap(rocket.getRocket(), startX1, startY1, paint);
+            canvas.drawBitmap(rocket.getRocket(), rocket.x, rocket.y, paint);
 
             getHolder().unlockCanvasAndPost(canvas);
         }
@@ -106,5 +153,46 @@ public class GameView extends SurfaceView implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                float deltaX = x2 - x1;
+                float deltaY = y2 - y1;
+
+                if (Math.abs(deltaX) > MIN_DISTANCE) {
+                    if (x1 > x2) { // Swipe esquerda para a direita
+                        rocket.isGoingLeft = true;
+                        Toast.makeText(getContext(), "Left to Right swipe [Next]", Toast.LENGTH_SHORT).show();
+                    } else { //Swipe direita para a esquerda
+                        rocket.isGoingLeft = false;
+                        rocket.isGoingRight = true;
+                        Toast.makeText(getContext(), "Right to Left swipe [Previous]", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if (Math.abs(deltaY) > MIN_DISTANCE) {
+                    if (y2 > y1) { // Swipe de baixo para cima
+                        rocket.isGoingUp = true;
+                        Toast.makeText(getContext(), "Down to Up swipe", Toast.LENGTH_SHORT).show();
+                    } else { // Swipe de cima para baixo
+                        rocket.isGoingUp = false;
+                        rocket.isGoingDown = true;
+                        Toast.makeText(getContext(), "Up to Down swipe", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return true;
     }
 }
--- SpaceCadet/app/src/main/java/com/example/spacecadet/Rocket.java
+++ SpaceCadet/app/src/main/java/com/example/spacecadet/Rocket.java
@@ -4,6 +4,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.util.Log;
 
 import static com.example.spacecadet.GameView.screenRatioX;
 import static com.example.spacecadet.GameView.screenRatioY;
@@ -13,8 +14,12 @@ public class Rocket {
     int x = 0, y = 0, width, height;
     Bitmap rocket;
     private GameView gameView;
+    boolean isGoingLeft = false;
+    boolean isGoingRight = false;
+    boolean isGoingUp = false;
+    boolean isGoingDown = false;
 
-    public Rocket(GameView gameView, int screenX, Resources res) {
+    public Rocket(GameView gameView, int screenX, int screenY, Resources res) {
         this.gameView = gameView;
 
         rocket = BitmapFactory.decodeResource(res, R.drawable.rocket);
@@ -22,13 +27,16 @@ public class Rocket {
         width = rocket.getWidth();
         height = rocket.getHeight();
 
-        width /= 3;
-        height /= 1.5;
+        width /= 6;
+        height /= 5;
 
         width = (int) (width * screenRatioX);
         height = (int) (height * screenRatioY);
 
         rocket = Bitmap.createScaledBitmap(rocket, width, height, false);
+
+        x = screenX / 2;
+        y = screenY / 2;
     }
 
     public Bitmap getRocket() {
