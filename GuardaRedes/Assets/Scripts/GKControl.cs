af7e0b27baf4ccf7375d1b810616d4ec9cdec8ac
goncaloLobo <g.ferreiralobo@gmail.com>
2020-03-02 15:27:01 +0000

    GuardaRedes -> Início.
    Som do remate escolhido.

---
+++ GuardaRedes/Assets/Scripts/GKControl.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class GKControl : MonoBehaviour
+{
+
+    public float maxSpawnRateInSeconds = 10f;
+    public AudioSource shotRight;
+    public AudioClip impact;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void KickBall()
+    {
+        // 1 - esquerda, 2 - centro, 3 - direita
+        //int shotRoll = Random.Range(1, 4);
+        int shotRoll = 3;
+        switch (shotRoll)
+        {
+            case 1:
+                Debug.Log("esquerda");
+                break;
+            case 2:
+                Debug.Log("centro");
+                break;
+            case 3:
+                Debug.Log("direita");
+                shotRight.loop = true;                
+                shotRight.Play();
+
+                break;
+        }
+
+        //Chutar novamente em tempo aleatório
+        //ScheduleBallKicks();
+
+        //Chutar novamente de 4 em 4 segundos
+        //ScheduleNextEnemySpawnFourSeconds();
+    }
+
+    // começar os remates
+    public void ScheduleKicks(float initialWaitTime)
+    {
+        Invoke("KickBall", initialWaitTime);
+    }
+
+    // chuta a bola entre 3 a 10s depois do chuto anterior
+    void ScheduleBallKicks()
+    {
+        float spawnInNSeconds;
+        if (maxSpawnRateInSeconds > 1f)
+        {
+            // o proximo inimigo vai aparecer Random.Range(1f, maxSpawnRateInSeconds) depois (>1 e <5)
+            spawnInNSeconds = Random.Range(1f, maxSpawnRateInSeconds);
+        }
+        else
+            spawnInNSeconds = 1f;
+
+        Invoke("KickBall", spawnInNSeconds);
+    }
+
+    // parar de chutar bolas
+    public void UnscheduleEnemySpawner()
+    {
+        CancelInvoke("SpawnEnemy");
+        CancelInvoke("IncreaseSpawnRate");
+    }
+}
---
+++ GuardaRedes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GKControl;
+    public GameObject playButton;
+
+    public enum GameManagerState
+    {
+        Opening, Gameplay, GameOver
+    }
+
+    GameManagerState GMState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GMState = GameManagerState.Opening;
+    }
+
+    void UpdateGameManagerState()
+    {
+        switch (GMState)
+        {
+            case GameManagerState.Opening:
+                playButton.SetActive(true);
+                break;
+            
+            case GameManagerState.Gameplay:
+                playButton.SetActive(false);
+                GKControl.GetComponent<GKControl>().ScheduleKicks(2);
+
+                break;
+            case GameManagerState.GameOver:
+                //mudar o estado do gamemanagerstate
+                Invoke("ChangeToOpeningState", 8f);
+
+                break;
+        }
+    }
+
+    public void SetGameManagerState(GameManagerState state)
+    {
+        GMState = state;
+        UpdateGameManagerState();
+    }
+
+    // botao play ira chamar esta funcao
+    public void StartGamePlay()
+    {
+        GMState = GameManagerState.Gameplay;
+        UpdateGameManagerState();
+    }
+
+    public void ChangeToOpeningState()
+    {
+        SetGameManagerState(GameManagerState.Opening);
+    }
+}
