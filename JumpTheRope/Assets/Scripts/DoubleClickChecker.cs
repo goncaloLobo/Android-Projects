7eeceff79109a8d2496055ae3e08d9140b568adf
goncaloLobo <g.ferreiralobo@gmail.com>
2020-03-16 17:00:59 +0000

    Rope -> Nothing new.

--- JumpTheRope/Assets/Scripts/DoubleClickChecker.cs
+++ JumpTheRope/Assets/Scripts/DoubleClickChecker.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class DoubleClickChecker : MonoBehaviour
 {
-    private bool tap, doubleTap;
+    private bool tap, doubleTap, notDoubleTap;
     private Vector2 startTouch;
-    private float doubleTapDelta = 0.2f;
+    private float doubleTapDelta = 0.5f;
     private float lastTap;
+    float touchDuration;
+    Touch touch;
 
-    public AudioSource audioData; // ManJumping
+    public AudioSource[] sounds;
+    public AudioSource manJumping; // ManJumping
+    public AudioSource buzzer; // Buzzer
 
     public bool Tap { get { return tap; } }
     public bool DoubleTap { get { return doubleTap; } }
@@ -15,12 +20,14 @@ public class DoubleClickChecker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioData = GetComponent<AudioSource>();        
+        sounds = GetComponents<AudioSource>();
+        manJumping = sounds[0];
+        buzzer = sounds[1];
     }
 
     void Update()
     {
-        tap = doubleTap = false;
+        tap = doubleTap = notDoubleTap = false;
 
 #if UNITY_EDITOR
         Update_Standalone();
@@ -34,47 +41,50 @@ public class DoubleClickChecker : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             tap = true;
-            startTouch = Input.mousePosition;
             doubleTap = Time.time - lastTap < doubleTapDelta;
             lastTap = Time.time;
         }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            startTouch = Vector2.zero;
-        }
 
-        if (tap)
-            Debug.Log("tap aqui!!");
-
+        // se for duplo toque então o homem salta.
         if (doubleTap && GameManager.GetStarted())
         {
-            Debug.Log("doubletap!!");
-            audioData.Play();
-        }            
+            manJumping.Play();
+            Debug.Log("iiia.");
+        }
+
+        // se não for um duplo toque bem feito, então o homem tropeça.
+        if (!doubleTap && GameManager.GetStarted() && tap)
+        {
+            //manJumping.Play();
+            buzzer.Play();
+            Debug.Log("doubletap: " + doubleTap + " tap: " + tap);
+        }
     }
 
     private void Update_Mobile()
     {
-        if(Input.touches.Length != 0)
+        if (Input.touches.Length != 0)
         {
-            if(Input.touches[0].phase == TouchPhase.Began)
+            if (Input.touches[0].phase == TouchPhase.Began)
             {
                 tap = true;
-                startTouch = Input.touches[0].position;
                 doubleTap = Time.time - lastTap < doubleTapDelta;
                 lastTap = Time.time;
             }
             else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
             {
-                startTouch = Vector2.zero;
+
             }
 
+            // se for tap deve meter um som de bzzzt
             if (tap)
-                Debug.Log("tap!!");
+            {
+                buzzer.Play();
+            }
 
             if (doubleTap && GameManager.GetStarted())
             {
-                audioData.Play();
+                manJumping.Play();
             }
         }
     }
