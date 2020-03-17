e90f71a3f1fe2a6be6226921791d5317435ef7d9
goncaloLobo <g.ferreiralobo@gmail.com>
2020-03-17 10:38:36 +0000

    GR -> Modos de Jogo (Swipes apenas)

--- GuardaRedes/Assets/Scripts/GameManager.cs
+++ GuardaRedes/Assets/Scripts/GameManager.cs
@@ -5,7 +5,13 @@ public class GameManager : MonoBehaviour
 {
     public GameObject playerShip;
     public GameObject playButton;
+    public GameObject howToButton;
     public GameObject GameOverGO;
+    public GameObject defenderCima;
+    public GameObject defenderBaixo;
+    public GameObject defenderDireita;
+    public GameObject defenderEsquerda;
+    public GameObject textGameModes;
 
     float clicked = 0;
     float clicktime = 0;
@@ -36,11 +42,14 @@ public class GameManager : MonoBehaviour
             case GameManagerState.Opening:
                 GameOverGO.SetActive(false);
                 playButton.SetActive(true);
+                howToButton.SetActive(true);
 
                 break;
             case GameManagerState.Gameplay:
 
                 playButton.SetActive(false);
+                howToButton.SetActive(false);
+                textGameModes.SetActive(false);
                 started = true;
 
                 // countdown para a velocidade
@@ -60,23 +69,63 @@ public class GameManager : MonoBehaviour
                 break;
             case GameManagerState.Instructions:
                 GameOverGO.SetActive(false);
+                playButton.SetActive(false);
+                howToButton.SetActive(false);
+                textGameModes.SetActive(true);
+
+                defenderBaixo.SetActive(true);
+                defenderCima.SetActive(true);
+                defenderEsquerda.SetActive(true);
+                defenderDireita.SetActive(true);
 
                 break;
 
             case GameManagerState.SwipeDown:
                 // caso para ensinar remates para baixo
+
+                defenderBaixo.SetActive(false);
+                defenderCima.SetActive(false);
+                defenderEsquerda.SetActive(false);
+                defenderDireita.SetActive(false);
+                textGameModes.SetActive(false);
+                started = true;
+
                 break;
 
             case GameManagerState.SwipeLeft:
                 // caso para ensinar remates para esquerda
+
+                defenderBaixo.SetActive(false);
+                defenderCima.SetActive(false);
+                defenderEsquerda.SetActive(false);
+                defenderDireita.SetActive(false);
+                textGameModes.SetActive(false);
+                started = true;
+
                 break;
 
             case GameManagerState.SwipeRight:
                 // caso para ensinar remates para direita
+
+                defenderBaixo.SetActive(false);
+                defenderCima.SetActive(false);
+                defenderEsquerda.SetActive(false);
+                defenderDireita.SetActive(false);
+                textGameModes.SetActive(false);
+                started = true;
+
                 break;
 
             case GameManagerState.SwipeUp:
                 // caso para ensinar remates para baixo
+
+                defenderBaixo.SetActive(false);
+                defenderCima.SetActive(false);
+                defenderEsquerda.SetActive(false);
+                defenderDireita.SetActive(false);
+                textGameModes.SetActive(false);
+                started = true;
+
                 break;
         }
     }
@@ -95,6 +144,110 @@ public class GameManager : MonoBehaviour
             clicked = 0;
             clicktime = 0;
             GMState = GameManagerState.Gameplay;
+            UpdateGameManagerState();
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
+
+    public void StartGameInstructions()
+    {
+        // Detecting double click
+        clicked++;
+
+        if (clicked == 1)
+            clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            // Double click detected
+            clicked = 0;
+            clicktime = 0;
+            GMState = GameManagerState.Instructions;
+            UpdateGameManagerState();
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
+
+    //funcao que vai ser chamada qdo o botao defender para cima é carregado
+    public void StartDefenderCima()
+    {
+        // Detecting double click
+        clicked++;
+
+        if (clicked == 1)
+            clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            // Double click detected
+            clicked = 0;
+            clicktime = 0;
+            GMState = GameManagerState.SwipeUp;
+            UpdateGameManagerState();
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
+
+    //funcao que vai ser chamada qdo o botao defender para baixo é carregado
+    public void StartDefenderBaixo()
+    {
+        // Detecting double click
+        clicked++;
+
+        if (clicked == 1)
+            clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            // Double click detected
+            clicked = 0;
+            clicktime = 0;
+            GMState = GameManagerState.SwipeDown;
+            UpdateGameManagerState();
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
+
+    //funcao que vai ser chamada qdo o botao defender para direita é carregado
+    public void StartDefenderDireita()
+    {
+        // Detecting double click
+        clicked++;
+
+        if (clicked == 1)
+            clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            // Double click detected
+            clicked = 0;
+            clicktime = 0;
+            GMState = GameManagerState.SwipeRight;
+            UpdateGameManagerState();
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
+
+    //funcao que vai ser chamada qdo o botao defender para esquerda é carregado
+    public void StartDefenderEsquerda()
+    {
+        // Detecting double click
+        clicked++;
+
+        if (clicked == 1)
+            clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            // Double click detected
+            clicked = 0;
+            clicktime = 0;
+            GMState = GameManagerState.SwipeLeft;
             UpdateGameManagerState();
         }
         else if (clicked > 2 || Time.time - clicktime > 1)
